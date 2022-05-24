79a00b6114636b3685b1ab4d0b829e1d7cad6706
LAK <email@lak.com>
2022-05-24 16:08:12 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home';
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+import { AdminRoom} from './pages/AdminRoom';
 
 import { AuthContenxtProvider } from './contexts/AuthContext'
 
@@ -12,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}  />
           <Route path="/rooms/new" element={<NewRoom />} />
+          <Route path="/rooms/:id" element={<Room />} />
+
+          <Route path="/admin/rooms/:id" element={<AdminRoom />} />
         </Routes>
       </AuthContenxtProvider>
     </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+
+import { database } from '../services/firebase';
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
@@ -13,12 +16,36 @@ export function Home(){
   const navigate = useNavigate();
   const { user, signInWithGoogle} = useAuth()
 
+  const [roomCode, setRoomCode] = useState('')
+
   async function handleCreateRoom(){
     if(!user){
       await signInWithGoogle()
     }
 
     navigate('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent){
+    event.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){
+      alert('Room does not exists.');
+      return;
+    }
+
+    if(roomRef.val().endedAt){
+      alert('Room alredy closed.');
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
   }
 
   return (
@@ -36,10 +63,12 @@ export function Home(){
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
